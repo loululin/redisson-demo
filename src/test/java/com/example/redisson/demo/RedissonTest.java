0c4df8baede606227d3647aad3d4f8aa2f81d7bc
娄绿林 (Bourne) <lvlin.lou@geely.com>
2021-11-10 15:56:27 +0800

    CountDownLatch

--- src/test/java/com/example/redisson/demo/RedissonTest.java
+++ src/test/java/com/example/redisson/demo/RedissonTest.java
@@ -9,6 +9,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
 import java.nio.charset.StandardCharsets;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -40,6 +43,56 @@ public class RedissonTest {
         String key = "test";
         int slot = CRC16.crc16(key.getBytes(StandardCharsets.UTF_8)) % 16384;
         log.info("key test slot is : {}",slot);
+
+    }
+
+    @Test
+    public void countDownTest(){
+        final CountDownLatch latch = new CountDownLatch(2);
+        System.out.println("主线程开始执行…… ……");
+
+        //第一个子线程执行
+        ExecutorService es1 = Executors.newSingleThreadExecutor();
+
+        es1.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(3000);
+                    System.out.println("子线程："+Thread.currentThread().getName()+"执行");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                latch.countDown();
+            }
+        });
+
+        es1.shutdown();
+
+        ExecutorService es2 = Executors.newSingleThreadExecutor();
+
+        es2.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(3000);
+                    System.out.println("子线程："+Thread.currentThread().getName()+"执行");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                latch.countDown();
+            }
+        });
+
+
+        es2.shutdown();
+        System.out.println("等待两个线程执行完毕…… ……");
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("两个子线程都执行完毕，继续执行主线程");
 
     }
 }
